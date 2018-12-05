86e1a6a886500c6fecc7f64a6d021f1c937f9cf2
Bhavik Gosrani <bhavik4748@gmail.com>
2018-12-05 09:04:27 -0500

    get customers method

---
+++ routes/customers.js
@@ -0,0 +1,20 @@
+const errors = require('restify-errors');
+const Customer = require('../models/Customer');
+
+
+module.exports = server => {
+
+    //Get Customers
+    server.get('/customers', async (req, res, next) => {
+
+        try {
+            const customers = await Customer.find({});
+            res.send(customers);
+            next();
+        } catch (err) {
+            return next(new errors.InvalidContentError(err));
+        }
+
+
+    });
+};
